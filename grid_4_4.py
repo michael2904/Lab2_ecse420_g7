39d82b1aa8c43331d6e63008ea1dfaa68328eb35
michael2904 <michael.abdallah.minciotti@gmail.com>
2016-11-03 11:01:25 -0400

    finished part 2 of the finite element

--- grid_4_4.py
+++ grid_4_4.py
@@ -49,15 +49,15 @@ for itera in range(T):
 	# print("This is rank "+str(rank)+" and dataN "+str(dataN))
 	dataR = comm.scatter(dataN,root = 0)
 	print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the data received "+str(dataR))
-	result1 = None
+	result = None
 	for i in range(1,N-1):
 		for j in range(1,N-1):
 			if rank == (i+(N-2)*j) % size:
-				result1 = (p * (dataR[0] + dataR[1] + dataR[2] + dataR[3] - 4 * dataR[4]) + 2 * dataR[4] - (1-eta) * dataR[5]) / (1+eta)
-				# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the result1 "+str(result1)+" at i,j "+str(i)+","+str(j))
+				result = (p * (dataR[0] + dataR[1] + dataR[2] + dataR[3] - 4 * dataR[4]) + 2 * dataR[4] - (1-eta) * dataR[5]) / (1+eta)
+				# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the result "+str(result)+" at i,j "+str(i)+","+str(j))
 
 
-	results = comm.gather(result1, root = 0)
+	results = comm.gather(result, root = 0)
 	print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the results 1 "+str(results))
 	if rank == 0:
 		for i in range(1,N-1):
@@ -73,13 +73,13 @@ for itera in range(T):
 
 	if rank == 0:
 		dataN1 = [None for _ in range(size)]
-		print("This is rank "+str(rank)+" and dataN1 "+str(dataN1))
+		# print("This is rank "+str(rank)+" and dataN1 "+str(dataN1))
 		for i in range(1,N-1):
 			dataN1[(i + 0*(N-2)) % size] = (uM[1][i])
 			dataN1[(i + 1*(N-2)) % size] = (uM[N - 2][i])
 			dataN1[(i + 2*(N-2)) % size] = (uM[1][i])
 			dataN1[(i + 3*(N-2)) % size] = (uM[1][i])
-			print("This is rank "+str(rank)+" and dataN1 "+str(dataN1))
+			# print("This is rank "+str(rank)+" and dataN1 "+str(dataN1))
 	else:
 		dataN1 = None
 
@@ -87,19 +87,26 @@ for itera in range(T):
 	dataR1 = comm.scatter(dataN1,root = 0)
 	print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the data received 1 "+str(dataR1))
 
+	result1 = None
+	for i in range(1,N-1):
+		for j in range(0,N):
+			if rank == (i + j*(N-2)) % size:
+				result1 = G * dataR1
+				print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the result1 "+str(result1)+" at i,j "+str(i)+","+str(j))
 
-	# for i in range(1,N-1):
-	# 	if rank < (2 * N):
-	# 		if rank % 4 == 0:
-	# 			u[0][i] = G * u[1][i]
-	# 		elif rank % 4 == 1:
-	# 			u[N - 1][i] = G * u[N - 2][i]
-	# 		elif rank % 4 == 2:
-	# 			u[i][0] = G * u[i][1]
-	# 		elif rank % 4 == 3:
-	# 			u[i][N - 1] = G * u[i][N - 2]
+	results1 = comm.gather(result1, root = 0)
+	print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the results1 "+str(results1))
+	if rank == 0:
+		for i in range(1,N-1):
+			uM[0][i] = results1[(i + 0*(N-2)) % size]
+			uM[N - 1][i] = results1[(i + 1*(N-2)) % size]
+			uM[i][0] = results1(i + 2*(N-2)) % size
+			uM[i][N - 1] = results1[(i + 3*(N-2)) % size]
 
-
+		for i in range(0,N):
+			for j in range(0,N):
+				print('u('+str(i)+","+str(j)+") : "+str(uM[i][j])+" |"),
+			print("")
 
 	# if rank < 4 :
 	# 	if rank % 4 == 0:
