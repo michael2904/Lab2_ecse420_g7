c2b33abd9596bb38e34317273fecdcff57d455b6
michael2904 <michael.abdallah.minciotti@gmail.com>
2016-11-08 16:53:27 -0500

    testing on big size

--- grid_4_4.py
+++ grid_4_4.py
@@ -48,7 +48,7 @@ for itera in range(T):
 					dataList.append(u1M[i][j+1])
 					dataList.append(u1M[i][j])
 					dataList.append(u2M[i][j])
-					dataN[((i-1)+(N-2)*(j-1)) / ((N-2)*(N-2)/size)].append(dataList)
+					dataN[((j-1)+(N-2)*(i-1)) / ((N-2)*(N-2)/size)].append(dataList)
 					# print(" ************* This is rank "+str(rank)+" and dataN "+str(dataN))
 				else:
 					dataList = []
@@ -58,7 +58,7 @@ for itera in range(T):
 					dataList.append(u1M[i][j+1])
 					dataList.append(u1M[i][j])
 					dataList.append(u2M[i][j])
-					dataN[((i-1)+(N-2)*(j-1))].append(dataList)
+					dataN[((j-1)+(N-2)*(i-1))].append(dataList)
 					# print(" ************* This is rank "+str(rank)+" and dataN "+str(dataN))
 	else:
 		dataN = None
