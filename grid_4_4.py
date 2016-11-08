306e4f286543ac0237dd248e100c7ac7b798bf08
michael2904 <michael.abdallah.minciotti@gmail.com>
2016-11-08 16:01:24 -0500

    test on server

--- grid_4_4.py
+++ grid_4_4.py
@@ -144,9 +144,19 @@ for itera in range(T):
 		for i in range(1,N-1):
 			count3Mod = count3%((4*(N-2)/size))
 			count3Val = count3/((4*(N-2)/size))
+			print("This is count3Mod: "+str(count3Mod)+" and this is count3Val: "+str(count3Val))
 			uM[0][i] = results1[count3Val][count3Mod]
+			count3 += 1
+			count3Mod = count3%((4*(N-2)/size))
+			count3Val = count3/((4*(N-2)/size))
 			uM[N - 1][i] = results1[count3Val][count3Mod]
+			count3 += 1
+			count3Mod = count3%((4*(N-2)/size))
+			count3Val = count3/((4*(N-2)/size))
 			uM[i][0] = results1[count3Val][count3Mod]
+			count3 += 1
+			count3Mod = count3%((4*(N-2)/size))
+			count3Val = count3/((4*(N-2)/size))
 			uM[i][N - 1] = results1[count3Val][count3Mod]
 			count3 += 1
 
