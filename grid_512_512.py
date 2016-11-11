d0684f90f16dc17b74f71eec9e443f3947ec38bd
michael2904 <michael.abdallah.minciotti@gmail.com>
2016-11-11 14:45:45 -0500

    added comments and good printing

--- grid_512_512.py
+++ grid_512_512.py
@@ -1,6 +1,10 @@
 # Lab 2 ECSE 420 group 7
 # Michael Abdallah-Minciotti
 # Marco Manglaviti
+# To build this code we wanted a process to have all the data together.
+# So we built it such that the first process has all the data, it sets it up for the computations and
+# scatters it to all the processes. After that each process runs the computation for its part of the data
+# and then the first process gathers it. It gets all the data and updates all the grids.
 # Lab 2 q2
 
 
@@ -9,11 +13,8 @@ from PIL import Image
 from math import exp
 from copy import copy,deepcopy
 import sys
-import time
 
 T = int(sys.argv[1])
-
-startTime = time.time()
 
 comm = MPI.COMM_WORLD
 size = MPI.COMM_WORLD.Get_size()
@@ -24,27 +25,31 @@ p = 0.5
 G = 0.75
 eta = 0.0002
 
-
-
-print("This is the rank "+str(rank)+" with name "+str(name))
-
 N = 512
+
+# This is the print to copy the format in output.h
+print("float output[2000] = {")
+
+
+# Here, I am setting up the 3 grids on the first process
 if rank == 0:
 	uM = [[0 for x in range(N)] for y in range(N)]
 	u1M = [[0 for x in range(N)] for y in range(N)]
 	u2M = [[0 for x in range(N)] for y in range(N)]
 	u1M[N/2][N/2] = 1
 
+# Here, I am looping for every iteration
 for itera in range(T):
-	# print("This is iteration "+str(itera)+" in rank "+str(rank))
+
 	# This is beginning of step 1
 	if rank == 0:
+		# dataN is the array where I am putting all the data need for each process do the computation
 		dataN = [[] for _ in range(size)]
-		# print(dataN)
+		# I am going through each point that I will need to compute and preparing the data for it
 		for i in range(1,N-1):
 			for j in range(1,N-1):
+				# Here I am verifiyng that the number of divisions of data is positive such that I do not divide by 0.
 				if ((N-2)*(N-2)/size) > 0:
-					# print("This is rank "+str(rank)+" and u1M "+str(u1M)+" and u1M[i-1][j] ="+str(u1M[i-1][j]))
 					dataList = []
 					dataList.append(u1M[i-1][j])
 					dataList.append(u1M[i+1][j])
@@ -52,7 +57,7 @@ for itera in range(T):
 					dataList.append(u1M[i][j+1])
 					dataList.append(u1M[i][j])
 					dataList.append(u2M[i][j])
-					# print(" ************* This is rank "+str(rank)+" and id is "+str((j-1)+(N-2)*(i-1)) +" / "+str(((N-2)*(N-2)/size)+ ((N-2)*(N-2) % size > 0))+" with result: "+str(((j-1)+(N-2)*(i-1))/(((N-2)*(N-2)/size)+ ((N-2)*(N-2) % size > 0))))
+					# For every point I sequentially add the data needed to the corresponding spot in the array for computation
 					dataN[((j-1)+(N-2)*(i-1))/(((N-2)*(N-2)/size)+ ((N-2)*(N-2) % size > 0))].append(dataList)
 				else:
 					dataList = []
@@ -63,72 +68,45 @@ for itera in range(T):
 					dataList.append(u1M[i][j])
 					dataList.append(u2M[i][j])
 					dataN[((j-1)+(N-2)*(i-1))].append(dataList)
-					# print(" ************* This is rank "+str(rank)+" and dataN "+str(dataN))
 	else:
 		dataN = None
 
-	# if rank == 0:
-	# 	print("This is rank "+str(rank)+" and dataN "+str(dataN))
+	# When I am done preparing the data, I scatter it through every process
 	dataR = comm.scatter(dataN,root = 0)
-	# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the data received "+str(dataR))
+
+	# Every process does the computation on its data
 	result = None
-	count = 0
 	resultList = []
 	for i in range(0,len(dataR)):
 		result = ((p * (dataR[i][0] + dataR[i][1] + dataR[i][2] + dataR[i][3] - 4 * dataR[i][4])) + (2 * dataR[i][4]) - ((1-eta) * dataR[i][5])) / (1+eta)
 		resultList.append(result)
-		# print("----------This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the results 1 "+str(resultList)+" with dataR "+str(dataR[count]))
 
-
+	# Here I am gathering back the computed data and saving it to the grid
 	results = comm.gather(resultList, root = 0)
-	# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the results 1 "+str(results))
 	if rank == 0:
-		# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the results 1 "+str(results))
 		count2 = 0
 		for i in range(1,N-1):
 			for j in range(1,N-1):
 				if ((N-2)*(N-2)/size) > 0 :
 					count2Mod = count2%(((N-2)*(N-2)/size) + ((N-2)*(N-2) % size > 0))
 					count2Val = count2/(((N-2)*(N-2)/size) + ((N-2)*(N-2) % size > 0))
-					# print("these are the indexes: "+str(count2Val)+" and "+str(count2Mod))
 					uM[i][j] = results[count2Val][count2Mod]
 				else:
 					uM[i][j] = results[count2][0]
 				count2 += 1
-
-
 	# This is the end of step 1
 
-	# if rank == 0:
-	# 	print("u")
-	# 	for i in range((N/2)-4,(N/2)+4):
-	# 		for j in range((N/2)-4,(N/2)+4):
-	# 			print('u('+str(i)+","+str(j)+") : "+str(uM[i][j])+" |"),
-	# 		print("")
-	# 	print("u1")
-	# 	for i in range((N/2)-4,(N/2)+4):
-	# 		for j in range((N/2)-4,(N/2)+4):
-	# 			print('u1('+str(i)+","+str(j)+") : "+str(u1M[i][j])+" |"),
-	# 		print("")
-	# 	print("u2")
-	# 	for i in range((N/2)-4,(N/2)+4):
-	# 		for j in range((N/2)-4,(N/2)+4):
-	# 			print('u2('+str(i)+","+str(j)+") : "+str(u2M[i][j])+" |"),
-	# 		print("")
-
 	# This is the beginning of step 2
+	# I use the same process as the first step to prepare the data for computation, scatter it, compute, gather and save.
 
 	if rank == 0:
 		dataN1 = [[] for _ in range(size)]
-		# print("This is rank "+str(rank)+" and dataN1 "+str(dataN1))
 		for i in range(1,N-1):
 			if (4*(N-2)/size) > 0 :
-				# print(" ************* This is rank "+str(rank)+" and id is "+str(((i-1)*4 + 0)) +" / "+str(((4*(N-2)/size)+ (4*(N-2) % size > 0)))+" with result: "+str(((i-1)*4 + 0) / ((4*(N-2)/size)+ (4*(N-2) % size > 0))))
 				dataN1[((i-1)*4 + 0) / ((4*(N-2)/size)+ (4*(N-2) % size > 0))].append(uM[1][i])
 				dataN1[((i-1)*4 + 1) / ((4*(N-2)/size)+ (4*(N-2) % size > 0))].append(uM[N - 2][i])
 				dataN1[((i-1)*4 + 2) / ((4*(N-2)/size)+ (4*(N-2) % size > 0))].append(uM[i][1])
 				dataN1[((i-1)*4 + 3) / ((4*(N-2)/size)+ (4*(N-2) % size > 0))].append(uM[i][N-2])
-				# print(">0*********This is rank "+str(rank)+" and dataN1 "+str(dataN1))
 			else:
 				dataN1[((i-1)*4 + 0)].append(uM[1][i])
 				dataN1[((i-1)*4 + 1)].append(uM[N - 2][i])
@@ -137,44 +115,34 @@ for itera in range(T):
 	else:
 		dataN1 = None
 
-	# if rank == 0:
-	# 	print("This is rank "+str(rank)+" and dataN1 "+str(dataN1))
-
 	dataR1 = comm.scatter(dataN1,root = 0)
-	# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the data received 1 "+str(dataR1))
 
 	result1 = None
 	result1List = []
 	for i in range(0,len(dataR1)):
 		result1 = G * dataR1[i]
 		result1List.append(result1)
-		# print("---------This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the result1List "+str(result1List)+" with this dataR1: "+str(dataR1))
 
 	results1 = comm.gather(result1List, root = 0)
-	# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the results1 "+str(results1))
+
 	if rank == 0:
-		# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the results1 "+str(results1))
 		count3 = 0
 		for i in range(1,N-1):
 			if (4*(N-2)/size) > 0 :
 				count3Mod = count3%((4*(N-2)/size) + (4*(N-2) % size > 0))
 				count3Val = count3/((4*(N-2)/size) + (4*(N-2) % size > 0))
-				# print("these are the indexes: "+str(count3Val)+" and "+str(count3Mod))
 				uM[0][i] = results1[count3Val][count3Mod]
 				count3 += 1
 				count3Mod = count3%((4*(N-2)/size) + (4*(N-2) % size > 0))
 				count3Val = count3/((4*(N-2)/size) + (4*(N-2) % size > 0))
-				# print("these are the indexes: "+str(count3Val)+" and "+str(count3Mod))
 				uM[N - 1][i] = results1[count3Val][count3Mod]
 				count3 += 1
 				count3Mod = count3%((4*(N-2)/size) + (4*(N-2) % size > 0))
 				count3Val = count3/((4*(N-2)/size) + (4*(N-2) % size > 0))
-				# print("these are the indexes: "+str(count3Val)+" and "+str(count3Mod))
 				uM[i][0] = results1[count3Val][count3Mod]
 				count3 += 1
 				count3Mod = count3%((4*(N-2)/size) + (4*(N-2) % size > 0))
 				count3Val = count3/((4*(N-2)/size) + (4*(N-2) % size > 0))
-				# print("these are the indexes: "+str(count3Val)+" and "+str(count3Mod))
 				uM[i][N - 1] = results1[count3Val][count3Mod]
 				count3 += 1
 			else:
@@ -189,25 +157,9 @@ for itera in range(T):
 
 	# This is the end of step 2
 
-	# if rank == 0:
-	# 	print("u")
-	# 	for i in range((N/2)-4,(N/2)+4):
-	# 		for j in range((N/2)-4,(N/2)+4):
-	# 			print('u('+str(i)+","+str(j)+") : "+str(uM[i][j])+" |"),
-	# 		print("")
-	# 	print("u1")
-	# 	for i in range((N/2)-4,(N/2)+4):
-	# 		for j in range((N/2)-4,(N/2)+4):
-	# 			print('u1('+str(i)+","+str(j)+") : "+str(u1M[i][j])+" |"),
-	# 		print("")
-	# 	print("u2")
-	# 	for i in range((N/2)-4,(N/2)+4):
-	# 		for j in range((N/2)-4,(N/2)+4):
-	# 			print('u2('+str(i)+","+str(j)+") : "+str(u2M[i][j])+" |"),
-	# 		print("")
-
 
 	# This is the beginning of step 3
+	# I use the same process as the first step to prepare the data for computation, scatter it, compute, gather and save.
 
 	if rank == 0:
 		dataN2 = [[] for _ in range(size)]
@@ -227,7 +179,6 @@ for itera in range(T):
 
 	
 	dataR2 = comm.scatter(dataN2,root = 0)
-	# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the data received 2 "+str(dataR2))
 
 	result2 = None
 	result2List = []
@@ -235,17 +186,13 @@ for itera in range(T):
 		result2 = G * dataR2[i]
 		result2List.append(result2)
 
-	# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the result2List "+str(result2List))
 	results2 = comm.gather(result2List, root = 0)
-	# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the results1 "+str(results2))
 
 	if rank == 0:
-		# print("This is iteration "+str(itera)+" in rank "+str(rank)+" and here is the results2 "+str(results2))
 		for i in range(0,4):
 			if (4*(N-2)/size) > 0 :
 				count5Mod = i%((4*(N-2)/size) + (4*(N-2) % size > 0))
 				count5Val = i/((4*(N-2)/size) + (4*(N-2) % size > 0))
-				# print("This is count5Val: "+str(count5Val)+" and this is count5Mod: "+str(count5Mod))
 				if i == 0:
 					uM[0][0] = results2[count5Val][count5Mod]
 				elif i == 1:
@@ -269,28 +216,9 @@ for itera in range(T):
 	if rank == 0:
 		u2M = deepcopy(u1M)
 		u1M = deepcopy(uM)
-		# print("u")
-		# for i in range((N/2)-4,(N/2)+4):
-		# 	for j in range((N/2)-4,(N/2)+4):
-		# 		print('u('+str(i)+","+str(j)+") : "+str(uM[i][j])+" |"),
-		# 	print("")
-		# print("u1")
-		# for i in range((N/2)-4,(N/2)+4):
-		# 	for j in range((N/2)-4,(N/2)+4):
-		# 		print('u1('+str(i)+","+str(j)+") : "+str(u1M[i][j])+" |"),
-		# 	print("")
-		# print("u2")
-		# for i in range((N/2)-4,(N/2)+4):
-		# 	for j in range((N/2)-4,(N/2)+4):
-		# 		print('u2('+str(i)+","+str(j)+") : "+str(u2M[i][j])+" |"),
-		# 	print("")
 		print("%.6f," % uM[N/2][N/2])
 
-endTime = time.time()
-delta = endTime - startTime
 
-
-print("TIME ELAPSED : " + str (delta) )
 
 
 
